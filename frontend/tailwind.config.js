550a93b94d2e3a6d8cdb96d049efb2cae31780e5
hnk005 <huynhnamkha512020@gmail.com>
2025-05-04 23:36:26 +0700

    chore: install tailwindcss and configure tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Quét toàn bộ file trong src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

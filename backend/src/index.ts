ca68347bc8f6c2374bf9cb69e9fdb9d38e7ea714
hnk005 <huynhnamkha512020@gmail.com>
2025-05-05 17:26:42 +0700

    #2 - run server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,19 @@
-// import process from "process";
-// import express, { Express } from "express";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import bodyParser from "body-parser";
-// import morgan from "morgan";
-// import cookieParser from "cookie-parser";
+import process from "process";
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 // import router from "./routers";
 // import { createClient } from "redis";
 // import errorMiddleware from "./middlewares/error.middleware";
 
-// dotenv.config();
+dotenv.config();
 
-// const app: Express = express();
-// const port = process.env.PORT || 3000;
+const app: Express = express();
+const port = process.env.PORT || 3000;
 
 // export const redisClient = createClient();
 
@@ -35,6 +35,6 @@
 // // Routes
 // app.use("/api", router);
 
-// app.listen(port, () => {
-//   console.log(`Server đang chạy tại http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server runing at http://localhost:${port}`);
+});

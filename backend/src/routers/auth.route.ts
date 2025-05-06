35a8956edbda26811b9d88e2c86d615bb72ac6bb
hnk005 <huynhnamkha512020@gmail.com>
2025-05-06 08:18:11 +0700

    #2 - validate route request

--- backend/src/routers/auth.route.ts
+++ backend/src/routers/auth.route.ts
@@ -1,9 +1,11 @@
 import { signin, signup } from "@/controllers/auth.controller";
+import validate from "@/middlewares/validate.middleware";
+import { SignInSchema, SignUpSchema } from "@/validators/auth.validator";
 import { Router } from "express";
 
 const authRoute = Router();
 
-authRoute.post("/sign-up", signup);
-authRoute.post("/sign-in", signin);
+authRoute.post("/sign-up", validate(SignUpSchema), signup);
+authRoute.post("/sign-in", validate(SignInSchema), signin);
 
 export default authRoute;

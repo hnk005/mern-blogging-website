2376705e783887c2d23294f5cff552437d5d4abc
hnk005 <huynhnamkha512020@gmail.com>
2025-05-31 15:21:02 +0700

    refactor: clean init firebase

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,13 +1,15 @@
 import admin from "firebase-admin";
-import dotenv from "dotenv";
-dotenv.config();
 
-const serviceAccount = JSON.parse(
-  Buffer.from(process.env.FIREBASE_ADMIN_CREDENTIAL, "base64").toString("utf8")
-);
+export const initAminFireBase = () => {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_ADMIN_CREDENTIAL, "base64").toString(
+      "utf8"
+    )
+  );
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 
-export default admin;
+  return admin;
+};

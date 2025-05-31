1bfdad407dba34c44f5ee6efcda52381bdaa7bd2
hnk005 <huynhnamkha512020@gmail.com>
2025-05-31 10:33:39 +0700

    fix: path

--- backend/src/config/firebaseAdmin.ts
+++ backend/src/config/firebaseAdmin.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 import type { ServiceAccount } from "firebase-admin";
-import rawServiceAccount from "../../mear-blogging-firebase-adminsdk-fbsvc-4c90cef65c.json";
+import rawServiceAccount from "mear-blogging-firebase-adminsdk-fbsvc-4c90cef65c.json";
 
 const serviceAccount = rawServiceAccount as ServiceAccount;
 
